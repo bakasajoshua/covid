e75b2ce1ce85d37b5d40655d6333d827cec9a949
Joel <joelkith@gmail.com>
2020-03-24 15:24:02 +0300

    Covid Dashboard

--- app/Covid.php
+++ app/Covid.php
@@ -8,9 +8,9 @@ use Str;
 class Covid
 {
     
-    public function edit_maps()
+    public static function edit_maps()
     {
-    	$counties = DB::connection('nat')->table('countys')->get();
+    	$counties = DB::table('countys')->get();
 
     	foreach ($counties as $county) {
     		$name = strtolower($county->name);
@@ -19,6 +19,21 @@ class Covid
 
     		$county_data = file_get_contents(public_path('maps/counties/' . $name . '.json'));
     		if(!$county_data) echo $name . " not found \n ";
+    		else{
+		    	$subcounties = DB::table('districts')->get();
+
+		    	foreach ($county_data->data->features as $key => $value) {
+		    		$subcounty = DB::table('districts')->where('name', $value->properties->name)->first();
+
+		    		if(!$subcounty) echo "Cannot find " . $value->properties->name . " in county {$name} \n";
+		    		else{
+		    			$value->properties->OBJECTID = $subcounty->id;
+		    			$county_data->data->features[$key] = $value;
+		    		}
+		    	}
+
+		    	file_put_contents(file_get_contents(public_path('maps/counties/' . $name . '.json')), $county_data);
+    		}
     	}
     }
 }
