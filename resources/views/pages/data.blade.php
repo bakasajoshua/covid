c59a4820d7cf79aa0205cb7b94c3c7b27f1bc7b4
Joel <joelkith@gmail.com>
2020-03-24 22:38:33 +0300

    Covid Dashboard

--- resources/views/pages/data.blade.php
+++ resources/views/pages/data.blade.php
@@ -7,7 +7,7 @@
 			</div>
 			<div class="row">
 				<div class="card p3 col-md-4">
-					<div class="p3"> Date: {{ date('Y-m-d') }} </div>
+					<div class="p3 m3"> Date: {{ date('Y-m-d') }} </div>
 					 
 				</div>
 				<div class="card p3 col-md-4"></div>
