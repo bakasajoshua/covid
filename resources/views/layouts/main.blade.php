49231e27cc36d92e3a9c3c91de30311d0fee580c
Joel <joelkith@gmail.com>
2020-04-03 02:37:07 +0300

    Completed Covid Testing

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -68,6 +68,7 @@
 								<!-- Nav Item - User Information -->
 								<li class="nav-item dropdown no-arrow">
 									<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
 										<span class="mr-2 d-none d-lg-inline text-gray-600 small"> {{ auth()->user()->name }} </span>
 										<!-- <img class="img-profile rounded-circle" src=""> -->
 									</a>
