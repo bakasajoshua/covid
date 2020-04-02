580304f1470ca3b6f5a9375955c18e400e210caf
Joel <joelkith@gmail.com>
2020-04-03 01:50:01 +0300

    Completed Covid Testing

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Hash;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -37,7 +38,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = Hash::make($value);
