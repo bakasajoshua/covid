94d1293495ad7147b154291aae055b27785ad26a
Joel <joelkith@gmail.com>
2020-04-03 02:26:33 +0300

    Completed Covid Testing

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,37 +47,44 @@
 					<!-- Topbar -->
 					<nav class="navbar navbar-expand navbar-light bg-white topbar mb-2 static-top shadow">
 
-					<!-- Sidebar Toggle (Topbar) -->
-					<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-						<i class="fa fa-bars"></i>
-					</button>
+						<!-- Sidebar Toggle (Topbar) -->
+						<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+							<i class="fa fa-bars"></i>
+						</button>
 
-					<!-- Topbar Navbar -->
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item text-dark">
-							<a class="nav-link text-dark font-weight-bold" href="/labs"> Labs</a>
-						</li>
-						
-						<div class="topbar-divider d-none d-sm-block"></div>
-
-						@auth
-							<!-- Nav Item - User Information -->
-							<li class="nav-item dropdown no-arrow">
-								<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									<span class="mr-2 d-none d-lg-inline text-gray-600 small"> {{ auth()->user()->name }} </span>
-									<!-- <img class="img-profile rounded-circle" src=""> -->
-								</a>
-								<!-- Dropdown - User Information -->
-								<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-									<a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-										<i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-										Logout
-									</a>
-								</div>
+						<ul class="nav nav-tabs">
+							<li class="nav-item text-dark">
+								<a class="nav-link text-dark font-weight-bold" href="/"> Home</a>
 							</li>
-						@endauth
+							<li class="nav-item text-dark">
+								<a class="nav-link text-dark font-weight-bold" href="/labs"> Labs</a>
+							</li>
+							
+						</ul>
 
-					</ul>
+						<!-- Topbar Navbar -->
+						<ul class="navbar-nav ml-auto">
+
+							<div class="topbar-divider d-none d-sm-block"></div>
+
+							@auth
+								<!-- Nav Item - User Information -->
+								<li class="nav-item dropdown no-arrow">
+									<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<span class="mr-2 d-none d-lg-inline text-gray-600 small"> {{ auth()->user()->name }} </span>
+										<!-- <img class="img-profile rounded-circle" src=""> -->
+									</a>
+									<!-- Dropdown - User Information -->
+									<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+										<a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+											<i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+											Logout
+										</a>
+									</div>
+								</li>
+							@endauth
+
+						</ul>
 
 					</nav>
 					<!-- End of Topbar -->
