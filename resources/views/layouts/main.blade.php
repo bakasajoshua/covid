d9fb73469768d6582cc63888df2e97129f9592eb
Joel <joelkith@gmail.com>
2020-04-03 02:35:31 +0300

    Completed Covid Testing

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,7 +19,9 @@
 	<!-- Custom fonts for this template-->
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-	<script src="https://use.fontawesome.com/5e1c4f503b.js"></script>
+	<!-- <script src="https://use.fontawesome.com/5e1c4f503b.js"></script> -->
+
+	<link href="/fontawesome-free/css/all.min.css" rel="stylesheet">
 
 	<!-- Custom styles for this template-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
