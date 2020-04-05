5010a74b62243cf545726640d899af032bd07943
Joel <joelkith@gmail.com>
2020-04-05 12:23:45 +0300

    Completed Covid Testing

---
+++ resources/views/charts/population.blade.php
@@ -0,0 +1,68 @@
+<div id="{{$div}}"></div>
+
+{!! $paragraph ?? '' !!}
+
+<script type="text/javascript">
+	
+    $(function () {
+        $('#{{$div}}').highcharts({
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+            title: {
+                text: ''
+            },
+            chart: {
+                type: 'bar'
+            },
+            accessibility: {
+                point:{
+                    valueDescriptionFormat: '{index}. Age {xDescription}, {value}%.'
+                }
+            },
+            xAxis: [
+            {
+                categories: {!! json_encode($categories ?? []) !!},
+                reversed: false,
+                accessibility:{
+                    description: 'Age (male)'
+                },
+            },
+            {
+                categories: {!! json_encode($categories ?? []) !!},
+                reversed: false,
+                opposite: true,
+                linkedTo: 0,
+                accessibility:{
+                    description: 'Age (female)'
+                },
+            }
+            ],
+            yAxis: {
+                title: {
+                    text: null
+                },
+                labels: {
+                    formatter: function() {
+                        return Math.abs(this.value);
+                    }
+                }
+            },
+
+            tooltip: {
+                formatter: function(){
+                    return '<b>' + this.series.name + ', age ' + this.point.category + '</b><br/>' + 'Population: ' + Highcharts.numberFormat(Math.abs(this.point.y), 1) + '%';
+                }
+            },
+            navigation: {
+                buttonOptions: {
+                    verticalAlign: 'bottom',
+                    y: -20
+                }
+            },
+            series: {!! json_encode($outcomes) !!}
+        });
+    });
+</script>
