639d7ef8284b08a21d5f6c280de8da66d174387b
Joel <joelkith@gmail.com>
2020-04-17 10:52:39 +0300

    Covid Bug Fixes

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -229,7 +229,7 @@ class ChartsController extends Controller
 	public function map_data()
 	{
 		$rows = CovidSampleView::selectRaw("county_id as id, countys.name, COUNT(DISTINCT covid_sample_view.patient_id) as value")
-			->leftJoin('countys', 'countys.id', '=', 'covid_sample_view.county_id')
+			->leftJoin('national_db.countys', 'countys.id', '=', 'covid_sample_view.county_id')
 			->where(['repeatt' => 0, 'result' => 2, 'test_type' => 1])
 			->groupBy('county_id')
 			->get();
