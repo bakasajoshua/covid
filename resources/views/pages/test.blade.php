c536a81af76214559b875279b7dde8acdb5cfdfe
Joel <joelkith@gmail.com>
2020-04-17 11:00:46 +0300

    Covid Bug Fixes

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -3,18 +3,26 @@
 @section('content')
 
 
-			<div class="row text-dark">
-				<div class="card mb-4 col-md-12">
-					<h5 class="card-header">Confirmed Cases By County</h5>
-					<div class="card-body">
-						<div class="row">
-							<div class="col-md-9" id="map_div">
-								
-							</div>
-						</div>	
-					</div>	
-				</div>			
-			</div>
+	<div class="row text-dark">
+		<div class="card mb-4 col-md-8">
+			<h5 class="card-header">Confirmed Cases By County</h5>
+			<div class="card-body">
+				<div class="row" id="map_div">
+
+				</div>	
+			</div>	
+		</div>
+
+		<div class="card mb-3 col-md-4">
+			<h5 class="card-header">Summarised Outcomes</h5>
+			<div class="card-body">
+				<div id="result_outcomes">
+					
+				</div>
+			</div>	
+		</div>						
+	</div>
+
 @endsection
 
 @section('scripts')
@@ -23,6 +31,7 @@
 	$(function() {
 
 		$("#map_div").load("/charts/map_data");
+		$("#result_outcomes").load("/charts/outcomes");
 
 	});
 </script>
