6e4b963dd663c7dd27de0a193daeaf107c6ca57a
Joshua Bakasa <baksajoshua09@gmail.com>
2020-05-04 02:44:59 +0300

    Require Covid consumables added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,11 +28,11 @@ class LoginController extends Controller
      * @var string
      */
     // protected $redirectTo = RouteServiceProvider::HOME;
-    protected $redirectTo = '/';
-    // protected function redirectTo()
-    // {
-    //     return $this->postLoginChecks();
-    // }
+    // protected $redirectTo = '/';
+    protected function redirectTo()
+    {
+        return $this->postLoginChecks();
+    }
 
     /**
      * Create a new controller instance.
