5683b4a97fbb413c7eea124d42cd894a995f27b5
Joshua Bakasa <baksajoshua09@gmail.com>
2020-05-07 21:33:20 +0300

    View Model functions

--- app/CovidSampleView.php
+++ app/CovidSampleView.php
@@ -20,8 +20,21 @@ class CovidSampleView extends BaseModel
     {
         return $this->belongsTo('App\CovidPatient', 'patient_id');
     }
-
     
+    /**
+     * Get the sample's result name
+     *
+     * @return string
+     */
+    public function getResultNameAttribute()
+    {
+        if($this->result == 1){ return "Negative"; }
+        else if($this->result == 2){ return "Positive"; }
+        else if($this->result == 3){ return "Failed"; }
+        else if($this->result == 5){ return "Collect New Sample"; }
+        else{ return ""; }
+    }
+
     /**
      * Get the patient's gender
      *
@@ -32,5 +45,10 @@ class CovidSampleView extends BaseModel
         if($this->sex == 1){ return "Male"; }
         else if($this->sex == 2){ return "Female"; }
         else{ return "No Gender"; }
+    }
+    
+    public function getSampletypeAttribute()
+    {
+        return DB::table('covid_test_types')->where('id', '=', "{$this->test_type}")->first()->name ?? null;
     }
 }
