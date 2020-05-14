cf3a11caf6f87cf212a658adf8fd47a9c717946a
Joel <joelkith@gmail.com>
2020-05-14 10:36:09 +0300

    Updates

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // $this->call(CovidLookupSeeder::class);
-        $this->call(CovidConsumptionSeeder::class);
+        $this->call(CovidLookupSeeder::class);
+        // $this->call(CovidConsumptionSeeder::class);
     }
 }
