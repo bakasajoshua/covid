3e4e8076373976c0f0868b94cd991ce0fc08f78b
Joel <joelkith@gmail.com>
2020-05-25 14:28:34 +0300

    Added Test Server. Completed API

--- app/Facility.php
+++ app/Facility.php
@@ -8,4 +8,12 @@ class Facility extends Model
 {
 
 	protected $table = 'facilitys';
+
+    public $timestamps = false;
+
+    public function scopeLocate($query, $param)
+    {
+    	if(is_numeric($param)) return $query->where('DHIScode', $param);
+    	 return $query->where('facilitycode', $param);
+    }
 }
