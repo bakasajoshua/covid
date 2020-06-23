4356afac03f035400796338d498beb0d5f591a9f
Joel <joelkith@gmail.com>
2020-06-23 09:11:04 +0300

    Request For Credentials

--- app/BaseModel.php
+++ app/BaseModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-    protected $guarded = ['id', 'created_at', 'updated_at', '_token', '_method', 'kemri_id', 'national_sample_id', 'national_patient_id', 'national_batch_id'];
+    protected $guarded = ['id', 'created_at', 'updated_at', '_token', '_method', 'kemri_id', 'national_sample_id', 'national_patient_id', 'national_batch_id', 'national_travel_id'];
 
     public function get_prop_name($coll, $attr, $attr2='name')
     {
